f877801591152c35db0105fb10d2fa67d817bc83
kelio <keliofilho12@gmail.com>
2025-02-03 16:22:38 -0300

    Department findAll

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -4,6 +4,8 @@ import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
+import java.util.List;
+
 public class Program2 {
 
     public static void main(String[] args) {
@@ -13,6 +15,12 @@ public class Program2 {
         System.out.println("=== TEST 1: findById =======");
         Department dep = departmentDao.findById(1);
         System.out.println(dep);
+
+        System.out.println("=== TEST 2: findAll =======");
+        List<Department> list = departmentDao.findAll();
+        for (Department d : list) {
+            System.out.println(d);
+        }
 
     }
 
